79ae697d6b0eeeb35c530961c8f51e3541592e68
Mariel <mariel.capulli@gmail.com>
2018-02-27 21:03:38 -0500

    library component checkpoint work

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Album extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
     );
   }
 
